6f4ecaf33717f38be11cfce20da5daabd8998d0b
Joshua James <josh452063@gmail.com>
2024-05-24 16:44:33 -0500

    tests passing, a couple of adjustments needed.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -4,10 +4,7 @@ import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -32,15 +29,15 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm){
     ArrayList<Job> jobs;
     if (searchType.equals("all")){
-        jobs = JobData.findAll();
-        model.addAttribute("title", "Jobs With All:" + searchTerm);
+        jobs = JobData.findByValue(searchTerm);
+        model.addAttribute("title", "Jobs With All: " + searchTerm);
     } else {
         jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
     }
     model.addAttribute("jobs", jobs);
 
-    return "list-jobs";
+    return "search";
 }
 }
 
